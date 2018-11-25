50f0402b48aac96d9e588c8eef58881763094fd8
Sebastián López Miranda <sebas.lopezm24@gmail.com>
2018-11-25 13:15:16 -0500

    React Components done

--- reactjs.js
+++ reactjs.js
@@ -234,3 +234,20 @@ const greatestDivEver = React.createElement(
   null,
   'i am div'
 );
+
+// First React Component
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+// component goes here:
+ReactDOM.render(
+  <MyComponentClass />,
+  document.getElementById('app')
+);
