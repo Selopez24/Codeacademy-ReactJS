f5973f27c38f51aabfa81ac30c6e753eb2b43cc0
Sebastián López Miranda <sebas.lopezm24@gmail.com>
2018-11-27 11:40:08 -0500

    Components render other components

--- reactjs.js
+++ reactjs.js
@@ -359,3 +359,38 @@ class Button extends React.Component {
 
 ReactDOM.render(<Button/>, document.getElementById('app'));
 
+/*
+ *    COMPONENTS RENDER OTHER COMPONENTS
+ */
+
+ // Apply a Component in a Render Function
+
+ export class NavBar extends React.Component {
+  render() {
+    const pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+    const navLinks = pages.map(page => {
+      return (
+        <a href={'/' + page}>
+          {page}
+        </a>
+      )
+    });
+
+    return <nav>{navLinks}</nav>;
+  }
+}
+
+ class ProfilePage extends React.Component {
+  render() {
+    return (
+      <div>
+				<NavBar/>
+        <h1>All About Me!</h1>
+        <p>I like movies and blah blah blah blah blah</p>
+        <img src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyselfie.jpg" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ProfilePage/>,document.getElementById('app') );
